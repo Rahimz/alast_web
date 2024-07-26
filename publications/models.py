0bcd66646c00d5fa3888c450b0a3ec9c0a156ca2
rahim <rahim.aghareb@gmail.com>
2024-07-26 19:04:50 +0330

    add Publication model

--- publications/models.py
+++ publications/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import FileExtensionValidator
+from django.utils.text import slugify
 
-# Create your models here.
+from tools.models import TimeStampedModel
+
+
+class Publication(TimeStampedModel):
+    title = models.CharField(
+        _("Title"),
+        max_length=250,
+    )
+    subtitle = models.CharField(
+        _("Sub title"),
+        max_length=250,
+        null=True,
+        blank=True
+    )
+    slug = models.SlugField(
+        allow_unicode=True,
+        null=True,
+        blank=True
+    )
+    authors = models.CharField(
+        _("Authors"),
+        max_length=350,
+    )
+    abstract = models.TextField(
+        _("Abstract"),
+        blank=True
+    )
+    pdf = models.FileField(
+        _("PDF file"),
+        upload_to='publications/pdf/',
+        validators=[FileExtensionValidator(['pdf'])]
+    )
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title, allow_unicode=True)
+        return super().save(*args, **kwargs)
--- publications/views.py
+++ publications/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
 
+from .models import Publication
+
 
 def PublicationsView(request):
     context = dict(
         page_title=_("Publications"),
-        # clients=Client.objects.all().prefetch_related('solutions'),
+        pubs=Publication.objects.all(),
         crumbs=[(_("Publications"), None),],
     )
     return render(
