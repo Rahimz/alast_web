0f3d2412700d7aa00b36819f9f85b7cb39bb3dd1
rahim <rahim.aghareb@gmail.com>
2024-07-26 17:27:31 +0330

    update solution in about us page

--- generals/views.py
+++ generals/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render
 from django.utils.translation import gettext_lazy as _
 
 from accounts.models import TeamMember
+from solutions.models import Solution
 
 def about(request):
     context = dict(
         page_title=_("About us"),
         crumbs=[(_("About us"), None),],
-        team_members = TeamMember.objects.all().order_by('rank')
+        team_members = TeamMember.objects.all().order_by('rank'),
+        solutions=Solution.objects.order_by('?')[:4]
     )
     return render(
         request,
