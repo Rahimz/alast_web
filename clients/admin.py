defc10feaf9b14dbaf0437d158b2760506dd294d
rahim <rahim.aghareb@gmail.com>
2024-11-06 08:33:38 +0330

    refactor solution admin page

--- clients/admin.py
+++ clients/admin.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 from .models import Client
 
 
-admin.register(Client)
+@admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
     list_display = ['id', 'name', ]
     prepopulated_fields = {"slug": ["name"]}
+    search_fields = ['name']
 
 
--- solutions/admin.py
+++ solutions/admin.py
@@ -1,15 +1,42 @@
 from django.contrib import admin
 
 from .models import Solution, SolutionCategory, Award
+from multimedias.models import Image
+
+class ImageGalleryInline(admin.StackedInline):
+    model = Image
+    raw_id_fields = ('solution',)
+    max_num = 2
+
 
 # admin.site.register(Solution)
-admin.site.register(SolutionCategory)
+@admin.register(SolutionCategory)
+class SolutionCategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', ]
+    search_fields = ['name']
 
 @admin.register(Solution)
 class SolutionAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'category', 'client']
-    search_fields = ['title', 'client__name','description', 'product_model', 'designer', 'manufacturing_methods', ]
+    search_fields = ['title', 'client', 'client__name','description', 'product_model', 'designer', 'manufacturing_methods', ]
     prepopulated_fields = {"slug": ["title"]}
+    
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'slug', 'category', 'client')
+        }),
+        ('Product Information', {
+            'fields': ('product_model', 'design_date', 'designer', 'manufacturing_methods', 'description', 'tags')
+        }),
+        ('Images', {
+            'fields': ('cover_image', 'thumbnail')
+        }),
+        ('Status', {
+            'fields': ('active',)
+        }),       
+    )
+    inlines = [ImageGalleryInline]
+    autocomplete_fields = ['client', 'category']
 
 
 @admin.register(Award)
