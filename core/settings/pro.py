18b8a75e4f1b972ef5950bd5da8cdadb5650c32e
rahim <rahim.aghareb@gmail.com>
2024-11-06 10:22:52 +0330

    add error handler

--- core/settings/pro.py
+++ core/settings/pro.py
@@ -1,3 +1,5 @@
+from logging.handlers import RotatingFileHandler
+
 from .base import *
 
 SECRET_KEY = pro_secret_key
@@ -21,3 +23,41 @@ CSRF_TRUSTED_ORIGINS = [
     'https://alastdesign.com',
     'https://www.alastdesign.com',
 ]
+
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+        'simple': {
+            'format': '{levelname} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'rotating_file': {
+            'level': 'ERROR',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'django_errors.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 10,  # Keep 10 backup files
+            'formatter': 'verbose',
+        },
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['rotating_file', 'console'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
